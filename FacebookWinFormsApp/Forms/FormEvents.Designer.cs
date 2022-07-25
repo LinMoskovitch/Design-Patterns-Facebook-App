3c5bbc0ea90f461f127204d0980b18467bc999a3
linkimos <linkimos@gmail.com>
2022-07-25 20:12:39 +0300

    Added events, need to check with Alon's profile

--- FacebookWinFormsApp/Forms/FormEvents.Designer.cs
+++ FacebookWinFormsApp/Forms/FormEvents.Designer.cs
@@ -29,19 +29,71 @@ namespace BasicFacebookFeatures.Forms
         /// </summary>
         private void InitializeComponent()
         {
+            this.listBoxEventsList = new System.Windows.Forms.ListBox();
+            this.pictureBoxEventsCoverPhoto = new System.Windows.Forms.PictureBox();
+            this.buttonFetchEvents = new System.Windows.Forms.Button();
+            this.panelAlbums = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEventsCoverPhoto)).BeginInit();
+            this.panelAlbums.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // listBoxEventsList
+            // 
+            this.listBoxEventsList.FormattingEnabled = true;
+            this.listBoxEventsList.ItemHeight = 25;
+            this.listBoxEventsList.Location = new System.Drawing.Point(107, 44);
+            this.listBoxEventsList.Name = "listBoxEventsList";
+            this.listBoxEventsList.Size = new System.Drawing.Size(507, 454);
+            this.listBoxEventsList.TabIndex = 3;
+            // 
+            // pictureBoxEventsCoverPhoto
+            // 
+            this.pictureBoxEventsCoverPhoto.Location = new System.Drawing.Point(769, 44);
+            this.pictureBoxEventsCoverPhoto.Name = "pictureBoxEventsCoverPhoto";
+            this.pictureBoxEventsCoverPhoto.Size = new System.Drawing.Size(481, 454);
+            this.pictureBoxEventsCoverPhoto.TabIndex = 2;
+            this.pictureBoxEventsCoverPhoto.TabStop = false;
+            // 
+            // buttonFetchEvents
+            // 
+            this.buttonFetchEvents.Location = new System.Drawing.Point(107, 551);
+            this.buttonFetchEvents.Name = "buttonFetchEvents";
+            this.buttonFetchEvents.Size = new System.Drawing.Size(204, 80);
+            this.buttonFetchEvents.TabIndex = 0;
+            this.buttonFetchEvents.Text = "Fetch events";
+            this.buttonFetchEvents.UseVisualStyleBackColor = true;
+            this.buttonFetchEvents.Click += new System.EventHandler(this.buttonFetchEvents_Click);
+            // 
+            // panelAlbums
+            // 
+            this.panelAlbums.Controls.Add(this.listBoxEventsList);
+            this.panelAlbums.Controls.Add(this.pictureBoxEventsCoverPhoto);
+            this.panelAlbums.Controls.Add(this.buttonFetchEvents);
+            this.panelAlbums.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelAlbums.Location = new System.Drawing.Point(0, 0);
+            this.panelAlbums.Name = "panelAlbums";
+            this.panelAlbums.Size = new System.Drawing.Size(1514, 794);
+            this.panelAlbums.TabIndex = 1;
             // 
             // FormEvents
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1514, 794);
+            this.Controls.Add(this.panelAlbums);
             this.Name = "FormEvents";
             this.Text = "Events";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEventsCoverPhoto)).EndInit();
+            this.panelAlbums.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBoxEventsList;
+        private System.Windows.Forms.PictureBox pictureBoxEventsCoverPhoto;
+        private System.Windows.Forms.Button buttonFetchEvents;
+        private System.Windows.Forms.Panel panelAlbums;
     }
 }
--- FacebookWinFormsApp/Forms/FormEvents.cs
+++ FacebookWinFormsApp/Forms/FormEvents.cs
@@ -13,9 +13,40 @@ namespace BasicFacebookFeatures.Forms
 {
     public partial class FormEvents : Form
     {
+        private readonly User r_LoggedInUser;
+
         public FormEvents(User i_LoggedInUser)
         {
+            r_LoggedInUser = i_LoggedInUser;
             InitializeComponent();
         }
+
+        private void buttonFetchEvents_Click(object i_Sender, EventArgs e)
+        {
+            fetchEvents();
+        }
+
+        private void fetchEvents()
+        {
+            listBoxEventsList.Items.Clear();
+            foreach(Album userAlbum in r_LoggedInUser.Albums)
+            {
+                listBoxEventsList.Items.Add(userAlbum);
+            }
+
+            if(listBoxEventsList.Items.Count == 0)
+            {
+                MessageBox.Show("No events to fetch.");
+            }
+        }
+
+        private void listBoxAlbumsList_SelectedValueChanged(object i_Sender, EventArgs e)
+            {
+                if (listBoxEventsList.Items.Count != 0)
+                {
+                    pictureBoxEventsCoverPhoto.LoadAsync((listBoxEventsList.SelectedItem as Album)?.PictureAlbumURL);
+                }
+            }
+        }
     }
-}
+
