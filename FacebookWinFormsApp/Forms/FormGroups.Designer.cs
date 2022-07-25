57b1888bdb4d19265aaaf238967a10fef1cfe299
linkimos <linkimos@gmail.com>
2022-07-25 20:40:15 +0300

    Added groups (works!) Events does not work, maybe because I dont know how to access it...

--- FacebookWinFormsApp/Forms/FormGroups.Designer.cs
+++ FacebookWinFormsApp/Forms/FormGroups.Designer.cs
@@ -29,12 +29,72 @@ namespace BasicFacebookFeatures.Forms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panelGroups = new System.Windows.Forms.Panel();
+            this.listBoxGroupsList = new System.Windows.Forms.ListBox();
+            this.pictureBoxGroupsCoverPhoto = new System.Windows.Forms.PictureBox();
+            this.buttonFetchGroups = new System.Windows.Forms.Button();
+            this.panelGroups.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGroupsCoverPhoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelGroups
+            // 
+            this.panelGroups.Controls.Add(this.listBoxGroupsList);
+            this.panelGroups.Controls.Add(this.pictureBoxGroupsCoverPhoto);
+            this.panelGroups.Controls.Add(this.buttonFetchGroups);
+            this.panelGroups.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelGroups.Location = new System.Drawing.Point(0, 0);
+            this.panelGroups.Name = "panelGroups";
+            this.panelGroups.Size = new System.Drawing.Size(1324, 756);
+            this.panelGroups.TabIndex = 1;
+            // 
+            // listBoxGroupsList
+            // 
+            this.listBoxGroupsList.FormattingEnabled = true;
+            this.listBoxGroupsList.ItemHeight = 25;
+            this.listBoxGroupsList.Location = new System.Drawing.Point(107, 44);
+            this.listBoxGroupsList.Name = "listBoxGroupsList";
+            this.listBoxGroupsList.Size = new System.Drawing.Size(507, 454);
+            this.listBoxGroupsList.TabIndex = 3;
+            this.listBoxGroupsList.SelectedValueChanged += new System.EventHandler(this.listBoxGroupsList_SelectedValueChanged);
+            // 
+            // pictureBoxGroupsCoverPhoto
+            // 
+            this.pictureBoxGroupsCoverPhoto.Location = new System.Drawing.Point(769, 44);
+            this.pictureBoxGroupsCoverPhoto.Name = "pictureBoxGroupsCoverPhoto";
+            this.pictureBoxGroupsCoverPhoto.Size = new System.Drawing.Size(481, 454);
+            this.pictureBoxGroupsCoverPhoto.TabIndex = 2;
+            this.pictureBoxGroupsCoverPhoto.TabStop = false;
+            // 
+            // buttonFetchGroups
+            // 
+            this.buttonFetchGroups.Location = new System.Drawing.Point(107, 551);
+            this.buttonFetchGroups.Name = "buttonFetchGroups";
+            this.buttonFetchGroups.Size = new System.Drawing.Size(204, 80);
+            this.buttonFetchGroups.TabIndex = 0;
+            this.buttonFetchGroups.Text = "Fetch groups";
+            this.buttonFetchGroups.UseVisualStyleBackColor = true;
+            this.buttonFetchGroups.Click += new System.EventHandler(this.buttonFetchGroups_Click);
+            // 
+            // FormGroups
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1324, 756);
+            this.Controls.Add(this.panelGroups);
+            this.Name = "FormGroups";
             this.Text = "Groups";
+            this.panelGroups.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGroupsCoverPhoto)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelGroups;
+        private System.Windows.Forms.ListBox listBoxGroupsList;
+        private System.Windows.Forms.PictureBox pictureBoxGroupsCoverPhoto;
+        private System.Windows.Forms.Button buttonFetchGroups;
     }
 }
--- FacebookWinFormsApp/Forms/FormGroups.cs
+++ FacebookWinFormsApp/Forms/FormGroups.cs
@@ -13,9 +13,40 @@ namespace BasicFacebookFeatures.Forms
 {
     public partial class FormGroups : Form
     {
+        private readonly User r_LoggedInUser;
+
         public FormGroups(User i_LoggedInUser)
         {
             InitializeComponent();
+            r_LoggedInUser = i_LoggedInUser;
+
+        }
+
+        private void buttonFetchGroups_Click(object i_Sender, EventArgs e)
+        {
+            fetchGroups();
+        }
+
+        private void fetchGroups()
+        {
+            listBoxGroupsList.Items.Clear();
+            foreach (Group userGroup in r_LoggedInUser.Groups)
+            {
+                listBoxGroupsList.Items.Add(userGroup);
+            }
+
+            if (listBoxGroupsList.Items.Count == 0)
+            {
+                MessageBox.Show("No groups to fetch.");
+            }
+        }
+
+        private void listBoxGroupsList_SelectedValueChanged(object i_Sender, EventArgs e)
+        {
+            if (listBoxGroupsList.Items.Count != 0)
+            {
+                pictureBoxGroupsCoverPhoto.LoadAsync((listBoxGroupsList.SelectedItem as Group)?.PictureNormalURL);
+            }
         }
     }
 }
