c6d107350cc574abbb31d4c0b275c25a4505ad82
linkimos <linkimos@gmail.com>
2022-08-06 18:30:54 +0300

    Added menu color list prop

--- FacebookWinFormsApp/Forms/FormPosts.cs
+++ FacebookWinFormsApp/Forms/FormPosts.cs
@@ -90,45 +90,6 @@ namespace BasicFacebookFeatures.Forms
             buttonPostNewStatus.Enabled = !textBoxStatus.Text.Equals(String.Empty);
         }
 
-        //private void buttonSortPosts_Click(object sender, EventArgs e)
-        //{
-        //    List<Post> sortedPostList;
-
-        //    if (listBoxPostsList.Items.Count != 0)
-        //    {
-        //        if (radioButtonSortByDate.Checked)
-        //        {
-        //            sortedPostList = listBoxPostsList.Items.Cast<Post>()
-        //                .OrderByDescending(i_Post => i_Post.CreatedTime).ToList();
-        //            listBoxPostsList.Items.Clear();
-        //            foreach (Post post in sortedPostList)
-        //            {
-        //                listBoxPostsList.Items.Add(post);
-        //            }
-        //        }
-        //        else if(radioButtonSortByLikes.Checked)
-        //        {
-        //            sortedPostList = listBoxPostsList.Items.Cast<Post>()
-        //                .OrderByDescending(i_Post => i_Post.LikedBy.Count).ToList();
-        //            listBoxPostsList.Items.Clear();
-        //            foreach (Post post in sortedPostList)
-        //            {
-        //                listBoxPostsList.Items.Add(post);
-        //            }
-        //        }
-        //        else
-        //        {
-        //            sortedPostList = listBoxPostsList.Items.Cast<Post>()
-        //                .OrderByDescending(i_Post => i_Post.Comments.Count).ToList();
-        //            listBoxPostsList.Items.Clear();
-        //            foreach (Post post in sortedPostList)
-        //            {
-        //                listBoxPostsList.Items.Add(post);
-        //            }
-        //        }
-        //    }
-        //}
-
         private void FormPosts_Load(object sender, EventArgs e)
         {
             UIThemeColor.LoadTheme(panelPosts);
